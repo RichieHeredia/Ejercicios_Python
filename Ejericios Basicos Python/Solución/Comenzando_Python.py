57d8046f6ece0715be30004dcb64921ae5eabe8c
RichieHeredia <richie.h.rodriguez@gmail.com>
2023-07-04 18:35:27 -0500

    Agregados los ejercicios de Python de listas
    Se crea la seccion de Listas a los ejericicios

---
+++ Ejericios Basicos Python/Solución/Comenzando_Python.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 1. LISTAS en Python
+# 
+# > Video: 
+
+# ### 1.1 Ejercicio de nivel fácil - Suma de elementos en una lista:
+# 
+# * Explicación del ejercicio: Dada una lista de números, se debe calcular la suma de todos los elementos.
+# 
+# * Parámetros de entrada:
+# 
+#  > **'lista_numeros' (lista)**: Una lista de números.
+# 
+# Ejemplo:
+
+# In[ ]:
+
+
+lista_numeros = [10, 20, 30, 40, 50]
+
+
+# ### Salida esperada:
+# La suma de los elementos es: 150
+
+# ### Solución: 
+
+# In[ ]:
+
+
+suma = sum(lista_numeros)
+print("La suma de los elementos es:", suma)
+
+
+# ### Ejercicio de nivel medio - Eliminación de elementos duplicados:
+# 
+# * Explicación del ejercicio: Dada una lista de números, se debe eliminar todos los elementos duplicados y obtener una lista con valores únicos.
+# 
+# * Parámetros de entrada:
+#     > **lista_numeros (lista):** Una lista de números.
+# 
+# Ejemplo:
+
+# In[ ]:
+
+
+lista_numeros = [10, 20, 10, 30, 40, 20, 50]
+
+
+# #### Salida esperada:
+# Lista sin duplicados: [40, 10, 50, 20, 30]
+
+# ### Solución
+
+# In[ ]:
+
+
+lista_sin_duplicados = list(set(lista_numeros))
+print("Lista sin duplicados:", lista_sin_duplicados)
+
+
+# ### Ejercicio de nivel difícil - Combinación de listas:
+# 
+# * Explicación del ejercicio: Dadas dos listas de nombres, se deben generar todas las combinaciones posibles entre los nombres de ambas listas. Cada combinación debe contener un nombre de la primera lista y un nombre de la segunda lista.
+# 
+# * Parámetros de entrada:
+# 
+#     > **lista_nombres1 (lista):** Una lista de nombres. <br>
+#     > **lista_nombres2 (lista):** Otra lista de nombres.
+#         
+# Ejemplo:
+
+# In[ ]:
+
+
+lista_nombres1 = ['Ana', 'Juan', 'Pedro']
+lista_nombres2 = ['María', 'Luis', 'Sofía']
+
+
+# ### Salida esperada:
+# 
+# Combinaciones de nombres:
+# 
+# ('Ana', 'María') <br> 
+# ('Ana', 'Luis') <br> 
+# ('Ana', 'Sofía') <br>
+# ('Juan', 'María') <br>
+# ('Juan', 'Luis')<br>
+# ('Juan', 'Sofía')<br>
+# ('Pedro', 'María')<br>
+# ('Pedro', 'Luis')<br>
+# ('Pedro', 'Sofía')
+
+# ### Solucion:
+
+# In[ ]:
+
+
+combinaciones = [(nombre1, nombre2) for nombre1 in lista_nombres1 for nombre2 in lista_nombres2]
+print("Combinaciones de nombres:")
+for combinacion in combinaciones:
+    print(combinacion)
+
